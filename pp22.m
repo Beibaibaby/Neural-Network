f40ca821b1955ffa2b0db5bdc73b7ee0e040e743
Draco Xu <1628407874@qq.com>
2021-04-28 23:04:30 +0800

    Update pp22.m

--- pp22.m
+++ pp22.m
@@ -1,24 +1,33 @@
 function [y] = MLPclassificationPredict(w,X,nHidden,nLabels)
 [nInstances,nVars] = size(X);
+nHiddenLayers = length(nHidden);
+
 % Form Weights
 inputWeights = reshape(w(1:nVars*nHidden(1)),nVars,nHidden(1));
 offset = nVars*nHidden(1);
-for h = 2:length(nHidden)
-  hiddenWeights{h-1} = reshape(w(offset+1:offset+nHidden(h-1)*nHidden(h)),nHidden(h-1),nHidden(h));
-  offset = offset+nHidden(h-1)*nHidden(h);
+for h = 2:nHiddenLayers
+    hiddenWeights{h-1} = reshape( w(offset+1:...
+        offset + (nHidden(h-1)+1)*nHidden(h) ), ...
+        nHidden(h-1)+1, nHidden(h));
+    offset = offset+(nHidden(h-1)+1)*nHidden(h);
 end
-outputWeights = w(offset+1:offset+nHidden(end)*nLabels);
-outputWeights = reshape(outputWeights,nHidden(end),nLabels);
+outputWeights = w(offset+1:offset+(nHidden(end)+1)*nLabels);
+outputWeights = reshape(outputWeights,nHidden(end)+1,nLabels);
 
+ip = cell(1, nHiddenLayers);
+fp = cell(1, nHiddenLayers);
+
+y = zeros(nInstances, nLabels);
 % Compute Output
 for i = 1:nInstances
-    ip{1} = X(i,:)*inputWeights;
+    ip{1} = [1, X(i,:)*inputWeights];
     fp{1} = tanh(ip{1});
     for h = 2:length(nHidden)
-        ip{h} = fp{h-1}*hiddenWeights{h-1};
+        ip{h} = [1, fp{h-1}*hiddenWeights{h-1}];
         fp{h} = tanh(ip{h});
     end
     y(i,:) = fp{end}*outputWeights;
 end
-[v,y] = max(y,[],2);
-%y = binary2LinearInd(y);
+[~,y] = max(y,[],2);
+
+end
