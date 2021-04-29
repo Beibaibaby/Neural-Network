f750ff974cdfe3d4ee00283c688068543844b3a4
Draco Xu <1628407874@qq.com>
2021-04-29 19:15:48 +0800

    Update t2t2.m

--- t2t2.m
+++ t2t2.m
@@ -1,90 +1,85 @@
-function [f,g,w] = MLP_finetune(w,X,y,nHidden,nLabels)
-% MLP_FINETUNE first overwrites outputWeights by least square method, and
-% then computes the error and gradients.
-%
-% Yuanbo Han, Dec. 9, 2017.
+function [f,g] = MLPclassificationLoss(w,X,y,nHidden,nLabels)
 
-[nInstances, nVars] = size(X);
-nHiddenLayers = length(nHidden);
+[nInstances,nVars] = size(X);
 
 % Form Weights
 inputWeights = reshape(w(1:nVars*nHidden(1)),nVars,nHidden(1));
-offset = nVars * nHidden(1);
-hiddenWeights = cell(1, nHiddenLayers-1);
-for h = 2:nHiddenLayers
-    hiddenWeights{h-1} = reshape(...
-        w(offset+1:offset+nHidden(h-1)*nHidden(h)),...
-        nHidden(h-1), nHidden(h));
-    offset = offset + nHidden(h-1) * nHidden(h);
+offset = nVars*nHidden(1);
+for h = 2:length(nHidden)
+    hiddenWeights{h-1} = reshape(w(offset+1:offset+nHidden(h-1)*nHidden(h)),nHidden(h-1),nHidden(h));
+    offset = offset+nHidden(h-1)*nHidden(h);
 end
-
-ip = cell(1, nHiddenLayers);
-fp = cell(1, nHiddenLayers);
+outputWeights = w(offset+1:offset+nHidden(end)*nLabels);
+outputWeights = reshape(outputWeights,nHidden(end),nLabels);
 
 f = 0;
-% Compute Output
-for i = 1:nInstances
-    ip{1} = X(i,:) * inputWeights;
-    fp{1} = tanh(ip{1});
-    for h = 2:length(nHidden)
-        ip{h} = fp{h-1} * hiddenWeights{h-1};
-        fp{h} = tanh(ip{h});
-    end
-    
-    % Compute outputWeights
-    outputWeights = pinv(fp{end}' * fp{end}) * fp{end}' * y(i,:);
-    w(offset+1:offset+nHidden(end)*nLabels) = outputWeights(:);
-    
-    yhat = fp{end} * outputWeights;
-    
-    relativeErr = yhat - y(i,:);
-    f = f + sum(relativeErr.^2);
-    
-    err = 2 * relativeErr;
-    
-    % Form Gradient
+% Form Graident
+if nargout > 1
     gInput = zeros(size(inputWeights));
-    gHidden = cell(1, nHiddenLayers-1);
-    for h = 2:nHiddenLayers
+    for h = 2:length(nHidden)
         gHidden{h-1} = zeros(size(hiddenWeights{h-1}));
     end
     gOutput = zeros(size(outputWeights));
+end
+
+% Compute Output
+for i = 1:nInstances
+    ip{1} = X(i,:)*inputWeights;
+    fp{1} = tanh(ip{1});
+    for h = 2:length(nHidden)
+        ip{h} = fp{h-1}*hiddenWeights{h-1};
+        fp{h} = tanh(ip{h});
+    end
+    yhat = fp{end}*outputWeights;
     
-    % Output Weights
-    gOutput = gOutput + fp{end}' * err;
+    relativeErr = yhat-y(i,:);
+    f = f + sum(relativeErr.^2);
     
-    if nHiddenLayers > 1
-        % Last Layer of Hidden Weights
-        backprop = (err' * sech(ip{end}).^2) .* outputWeights';
-        backprop = sum(backprop,1);
-        gHidden{end} = gHidden{end} + fp{end-1}' * backprop;
+    if nargout > 1
+        err = 2*relativeErr;
         
-        % Other Hidden Layers
-        for h = length(nHidden)-2:-1:1
-            backprop = (backprop * hiddenWeights{h+1}') .* ...
-                sech(ip{h+1}).^2;
-            gHidden{h} = gHidden{h} + fp{h}' * backprop;
+        % Output Weights
+        for c = 1:nLabels
+            gOutput(:,c) = gOutput(:,c) + err(c)*fp{end}';
         end
         
-        % Input Weights
-        backprop = (backprop * hiddenWeights{1}') .* sech(ip{1}).^2;
-        gInput = gInput + X(i,:)' * backprop;
+        if length(nHidden) > 1
+            % Last Layer of Hidden Weights
+            clear backprop
+            for c = 1:nLabels
+                backprop(c,:) = err(c)*(sech(ip{end}).^2.*outputWeights(:,c)');
+                gHidden{end} = gHidden{end} + fp{end-1}'*backprop(c,:);
+            end
+            backprop = sum(backprop,1);
+            
+            % Other Hidden Layers
+            for h = length(nHidden)-2:-1:1
+                backprop = (backprop*hiddenWeights{h+1}').*sech(ip{h+1}).^2;
+                gHidden{h} = gHidden{h} + fp{h}'*backprop;
+            end
+            
+            % Input Weights
+            backprop = (backprop*hiddenWeights{1}').*sech(ip{1}).^2;
+            gInput = gInput + X(i,:)'*backprop;
+        else
+            % Input Weights
+            for c = 1:nLabels
+                gInput = gInput + err(c)*X(i,:)'*(sech(ip{end}).^2.*outputWeights(:,c)');
+            end
+        end
         
-    else % nHiddenLayers == 1
-        % Input Weights
-        gInput = gInput + X(i,:)' * ...
-            ( sech(ip{end}).^2 .* (outputWeights * err')' );
     end
+    
 end
 
 % Put Gradient into vector
-g = zeros(size(w));
-g(1:nVars*nHidden(1)) = gInput(:);
-offset = nVars*nHidden(1);
-for h = 2:nHiddenLayers
-    g(offset+1:offset+nHidden(h-1)*nHidden(h)) = gHidden{h-1};
-    offset = offset+nHidden(h-1)*nHidden(h);
-end
-g(offset+1:offset+nHidden(end)*nLabels) = gOutput(:);
-
+if nargout > 1
+    g = zeros(size(w));
+    g(1:nVars*nHidden(1)) = gInput(:);
+    offset = nVars*nHidden(1);
+    for h = 2:length(nHidden)
+        g(offset+1:offset+nHidden(h-1)*nHidden(h)) = gHidden{h-1};
+        offset = offset+nHidden(h-1)*nHidden(h);
+    end
+    g(offset+1:offset+nHidden(end)*nLabels) = gOutput(:);
 end
